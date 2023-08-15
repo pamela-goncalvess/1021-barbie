755dd4e95c2657380fd755e5479f885a194cb420
PAMELA CRISTINA LEITE GONCALVES <07138416130@ifms.edu.br>
2023-08-15 07:34:00 -0400

    primeiro comit

---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,28 @@
+import './Header.css'
+
+function Header(){
+    return(
+        <header>
+            <div>  
+        Barbie
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+            </li>
+          <li>
+            <a href="#">Sobre</a>
+            </li>
+          <li>
+            <a href="#">Notícias</a>
+            </li>
+          <li>
+            <a href="#">Contato</a>
+            </li>
+        </ul>
+      </nav>
+        </header>
+    )
+}
+export default Header
